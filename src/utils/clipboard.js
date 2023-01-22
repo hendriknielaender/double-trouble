9c4c757346c9599f4a7458d19b0d64ffc5c3b12f
Hendrik Nieländer <hendrik@nielaender.de>
2023-01-22 23:11:38 +0100

    feat: add copy to clipboard

---
+++ src/utils/clipboard.js
@@ -0,0 +1,35 @@
+const copyButtonIcon = '<svg viewBox="0 0 24 24" width="22" height="22" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" fill="none" shape-rendering="geometricPrecision" style="color:currentcolor"><path d="M8 17.929H6c-1.105 0-2-.912-2-2.036V5.036C4 3.91 4.895 3 6 3h8c1.105 0 2 .911 2 2.036v1.866m-6 .17h8c1.105 0 2 .91 2 2.035v10.857C20 21.09 19.105 22 18 22h-8c-1.105 0-2-.911-2-2.036V9.107c0-1.124.895-2.036 2-2.036z"/></svg>';
+
+let blocks;
+if (typeof document !== "undefined") {
+    // use a class selector if available
+    blocks = document.querySelectorAll("pre");
+
+    blocks.forEach((block) => {
+      // only add button if browser supports Clipboard API
+      if (navigator.clipboard) {
+        let button = document.createElement("button");
+
+        button.innerHTML = copyButtonIcon;
+        block.appendChild(button);
+
+        button.addEventListener("click", async () => {
+          await copyCode(block, button);
+        });
+      }
+    });
+}
+
+export async function copyCode(block, button) {
+  let code = block.querySelector("code");
+  let text = code.innerText;
+
+  await navigator.clipboard.writeText(text);
+
+  // visual feedback that task is completed
+  button.innerText = "Copied";
+
+  setTimeout(() => {
+    button.innerHTML = copyButtonIcon;
+  }, 700);
+}
