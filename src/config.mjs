cc9782fdbd822c08d727907e6bcc5312677a8730
Felix Brilej <felix.brilej@taxdoo.com>
2023-03-19 11:46:35 +0100

    chore: run prettier

--- src/config.mjs
+++ src/config.mjs
@@ -1,38 +1,38 @@
 export const SITE = {
-	name: 'Touble Trouble',
+  name: "Touble Trouble",
 
-	origin: 'https://double-trouble.dev',
-	basePathname: '/',
-	trailingSlash: false,
+  origin: "https://double-trouble.dev",
+  basePathname: "/",
+  trailingSlash: false,
 
-	title: 'Double Trouble',
-	description: '🚀 Twice the fun, double the trouble.',
+  title: "Double Trouble",
+  description: "🚀 Twice the fun, double the trouble.",
 
-	googleAnalyticsId: false, // or "G-XXXXXXXXXX",
-	googleSiteVerificationId: 'orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M',
+  googleAnalyticsId: false, // or "G-XXXXXXXXXX",
+  googleSiteVerificationId: "orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M",
 };
 
 export const BLOG = {
-	disabled: false,
-	postsPerPage: 4,
+  disabled: false,
+  postsPerPage: 4,
 
-	blog: {
-		disabled: false,
-		pathname: 'blog', // blog main path, you can change this to "articles" (/articles)
-	},
+  blog: {
+    disabled: false,
+    pathname: "blog", // blog main path, you can change this to "articles" (/articles)
+  },
 
-	post: {
-		disabled: false,
-		pathname: '/post', // empty for /some-post, value for /pathname/some-post
-	},
+  post: {
+    disabled: false,
+    pathname: "/post", // empty for /some-post, value for /pathname/some-post
+  },
 
-	category: {
-		disabled: true,
-		pathname: 'category', // set empty to change from /category/some-category to /some-category
-	},
+  category: {
+    disabled: true,
+    pathname: "category", // set empty to change from /category/some-category to /some-category
+  },
 
-	tag: {
-		disabled: false,
-		pathname: 'tag', // set empty to change from /tag/some-tag to /some-tag
-	},
+  tag: {
+    disabled: false,
+    pathname: "tag", // set empty to change from /tag/some-tag to /some-tag
+  },
 };
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,14 +1,14 @@
-import rss from '@astrojs/rss';
+import rss from "@astrojs/rss";
 
-import { SITE, BLOG } from '~/config.mjs';
-import { fetchPosts } from '~/utils/posts';
-import { getPermalink } from '~/utils/permalinks';
+import { SITE, BLOG } from "~/config.mjs";
+import { fetchPosts } from "~/utils/posts";
+import { getPermalink } from "~/utils/permalinks";
 
 export const get = async () => {
   if (BLOG.disabled) {
     return new Response(null, {
       status: 404,
-      statusText: 'Not found',
+      statusText: "Not found",
     });
   }
 
@@ -20,7 +20,7 @@ export const get = async () => {
     site: import.meta.env.SITE,
 
     items: posts.map((post) => ({
-      link: getPermalink(post.slug, 'post'),
+      link: getPermalink(post.slug, "post"),
       title: post.title,
       description: post.description,
       pubDate: post.publishDate,
