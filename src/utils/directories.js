bb63e59d04576e5a14620036ad068f0553202e06
Hendrik Nieländer <hendrik@nielaender.de>
2024-02-01 22:05:10 +0100

    fix: apply biome formatter rules

--- src/utils/directories.js
+++ src/utils/directories.js
@@ -5,20 +5,20 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /** */
 export const getProjectRootDir = () => {
-  const mode = import.meta.env.MODE;
+	const mode = import.meta.env.MODE;
 
-  return mode === "production"
-    ? path.join(__dirname, "../")
-    : path.join(__dirname, "../../");
+	return mode === "production"
+		? path.join(__dirname, "../")
+		: path.join(__dirname, "../../");
 };
 
 const __srcFolder = path.join(getProjectRootDir(), "/src");
 
 /** */
 export const getRelativeUrlByFilePath = (filepath) => {
-  if (filepath) {
-    return filepath.replace(__srcFolder, "");
-  }
+	if (filepath) {
+		return filepath.replace(__srcFolder, "");
+	}
 
-  return null;
+	return null;
 };
